0ec90c7057f07c901ad340844870bd3f3cd897f7
ingriddomingos <113357688+ingriddomingos@users.noreply.github.com>
2022-10-26 15:59:52 -0300

    criando volumes no container do Oracle

--- oracle.sh
+++ oracle.sh
@@ -1,1 +1,5 @@
 docker run -d --name oracle -p 1521:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
+docker container logs -f oracle
+
+docker create volume oracle -volume
+docker run -d -v oracle-volume:/opt/oracle/oradate oracle -p 1521:1521 -e ORACLE_PASSWORD=admin gvenzl/oracle-xe
